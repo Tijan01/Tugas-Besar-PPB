38a43798299e16d379c043621b50600c0582109c
Tijan01 <muhammadtijan0@gmail.com>
2023-06-21 20:04:48 +0700

    Perubahan Tubes PPB K9

--- TugasBesarPPB/app/src/main/java/com/tubes/foodtracker/data/NutritionalValues.kt
+++ TugasBesarPPB/app/src/main/java/com/tubes/foodtracker/data/NutritionalValues.kt
@@ -19,6 +19,6 @@ data class NutritionalValues (val Calories : Float,
             Fat * b)
     companion object{
         val empty get() = NutritionalValues(0f,0f,0f,0f)
-        val recommended get() = NutritionalValues(2000f, 260f, 50f, 70f)
+        val recommended get() = NutritionalValues(2000f, 325f, 75f, 44f)
     }
 }
